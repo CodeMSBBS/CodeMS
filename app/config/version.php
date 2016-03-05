8325a6c5ef6e8c67467e9cefd2a8e82e5d4a3df0
codems <admin@codems.cn>
2016-03-05 21:52:35 +0800

    版本信息

--- app/config/version.php
+++ app/config/version.php
@@ -1,9 +1,9 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
 $config = array (
-  'sys_name' => 'StartBBS',
-  'sys_url' => 'http://www.startbbs.com',
-  'sys_version' => 'V1.3.0',
-  'sys_update' => '20150201',
-  'sys_author' => 'QQ858292510',
+  'sys_name' => 'CodeMs',
+  'sys_url' => 'http://www.codems.cn',
+  'sys_version' => 'V1.0.0',
+  'sys_update' => '20160305',
+  'sys_author' => 'Vincent H',
 );
